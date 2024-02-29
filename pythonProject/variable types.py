40aa5db197f6c07074bc073e4de4b84e5cb42588
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-02-29 14:34:29 +1300

    Create variable types.py

---
+++ pythonProject/variable types.py
@@ -0,0 +1,38 @@
+a = 42
+b = 3.14
+c = "Hello, World!"
+d = [1, 2, 3]
+e = (1, 2, 3)
+f = {"name": "John", "age": 30}
+# starting printing data types bellow
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+# done printing data types
+print(str(b))
+# converted b to string
+g=b**3
+print(g)
+# b to a power of three calculation
+h=a%b
+print(h)
+# a modulus b
+a+=2
+print(a)
+a-=2
+print(a)
+a*=2
+print(a)
+a/=2
+print(a)
+# assign add, assign subtract, assign mulƟple, and assign divide on a
+print(c.upper())
+# capitalising everything in c
+sus=str(a)+str(b)
+print(sus)
+# concatenating a and b
+print(c[::-1])
+# reversing c
