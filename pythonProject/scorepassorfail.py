51224a55b655f07869e7d23f1996b7730c1dbc2e
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-03-06 14:19:47 +1300

    Create scorepassorfail.py

---
+++ pythonProject/scorepassorfail.py
@@ -0,0 +1,9 @@
+m1=float(input("Input first mark"))
+m2=float(input("Input second mark"))
+m3=float(input("Input third mark"))
+m4=float(input("Input fourth mark"))
+sc=(m1+m2+m3+m4)/4
+if sc<50:
+    print("FAIL")
+else:
+    print("PASS")
