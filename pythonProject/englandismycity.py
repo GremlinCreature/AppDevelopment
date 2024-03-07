088b826e10aa3fe45701194ea6e57fa636290290
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-03-07 15:04:54 +1300

    Create englandismycity.py

---
+++ pythonProject/englandismycity.py
@@ -0,0 +1,10 @@
+city=[]
+while len(city)<5:
+    city.append(input("Input name of a city"))
+print(len(city))
+print(city)
+city.append("Hong")
+del city[0]
+print(city)
+city.reverse()
+print(city)
