37033da93b289a2c82164beb8c5efde8e0356945
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-03-07 14:44:10 +1300

    Update ifscore.py

--- pythonProject/ifscore.py
+++ pythonProject/ifscore.py
@@ -3,9 +3,14 @@ m2=float(input("Input second mark"))
 m3=float(input("Input third mark"))
 m4=float(input("Input fourth mark"))
 sc=(m1+m2+m3+m4)/4
-if sc>100 or sc<0:
-    print("Invalid range")
-elif sc>=80:
+while (sc>100 or sc<0):
+    print("Input error, try again")
+    m1 = float(input("Input first mark"))
+    m2 = float(input("Input second mark"))
+    m3 = float(input("Input third mark"))
+    m4 = float(input("Input fourth mark"))
+    sc = (m1 + m2 + m3 + m4) / 4
+if sc>=80:
     print("A")
 elif sc>=60:
     print("B")
