ffc51b1f57d8ecb33b3f04394e34dcfe0edca3dc
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-03-13 11:05:17 +1300

    Create arrayslicing.py

---
+++ pythonProject/arrayslicing.py
@@ -0,0 +1,13 @@
+arr = [1, 2, 3, 4, 5, 6, 7]
+print(arr[4:7])
+# slicing 1 to 5 from an aray
+print(arr[1:])
+#slicing without end index
+print(arr[:])
+#slicing without end and start index
+print(arr[:5])
+#slicing without start index
+print(arr[::2])
+#slicing with steps
+print(arr[-4:-1])
+#slicing with negative values
