d4ca44b5b932815e6c5516633f533702f89ae718
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-03-13 14:46:13 +1300

    Create vowels.py

---
+++ pythonProject/vowels.py
@@ -0,0 +1,13 @@
+m=input("Input your message: ")
+ml=list(m)
+v=['a','e','i','o','u']
+V=['A','E','I','O','U']
+counter=0
+for l in ml:
+    for l1 in v :
+        if l == l1:
+            counter=counter+1
+    for l2 in V:
+        if l==l2:
+            counter=counter+1
+print(counter)
