0df8484871440fa69acba391c97ee8533a0eccb4
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-03-14 14:39:14 +1300

    fixed an error with an "and" function

--- pythonProject/codeprint.py
+++ pythonProject/codeprint.py
@@ -3,15 +3,19 @@ fn=input('Input first name: ')
 ln=input('Input last name: ')
 m=input('Input "I am a newbie in Whitecliffe college: ')
 ms=m.split()
-if "Whitecliffe" in ms and "college" in ms :
-    code=id[:2]+ln[:3]
-    print(code)
-elif "whitecliffe" in ms and "college" in ms:
-    code = id[:2] + ln[:3]
-    print(code)
-elif "Whitecliffe" in ms and "College" in ms :
-    code=id[:2]+ln[:3]
-    print(code)
-elif "whitecliffe" in ms and "College" in ms :
-    code=id[:2]+ln[:3]
-    print(code)
+if "Whitecliffe" in ms:
+    if"college" in ms :
+        code=id[:2]+ln[:3]
+        print(code)
+elif "whitecliffe" in ms:
+    if "college" in ms:
+        code = id[:2] + ln[:3]
+        print(code)
+elif "Whitecliffe" in ms:
+    if "College" in ms :
+        code=id[:2]+ln[:3]
+        print(code)
+elif "whitecliffe" in ms:
+    if "College" in ms :
+        code=id[:2]+ln[:3]
+        print(code)
