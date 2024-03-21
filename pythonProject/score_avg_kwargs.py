b133099bfb57bf9285b9dbe0ee3e687f1d8119be
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-03-21 14:41:16 +1300

    Added validation system
    Now it checks if there are only numbers being put in.

--- pythonProject/score_avg_kwargs.py
+++ pythonProject/score_avg_kwargs.py
@@ -1,7 +1,10 @@
 def marks_avg(**marks):
     sum=0
-    for i in marks.values():
-        sum=sum+i
+    try:
+        for i in marks.values():
+            sum=sum+i
+    except TypeError:
+        return "Please, input only numbers."
     avg=sum/len(marks)
     return avg
-print(marks_avg(IT5014=60, IT7809=80, IT6798=50, IT5048=70))
+print(marks_avg(IT5014=60, IT7809="k", IT6798=50, IT5048=70))
