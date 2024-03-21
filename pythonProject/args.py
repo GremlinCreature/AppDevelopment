1ffb8da73e3427cd1ccf4d2d6e6124374c04e4ce
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-03-21 14:57:14 +1300

    Added type validation

--- pythonProject/args.py
+++ pythonProject/args.py
@@ -1,15 +1,21 @@
 def average_args(*gr):
     score=0
-    for i in gr:
-        score=score+i
-    avg=score/len(gr)
+    try:
+        for i in gr:
+            score=score+i
+        avg=score/len(gr)
+    except TypeError:
+        return "Please, input only numbers."
     return avg
 def pass_or_fail(score):
-    if score <50:
-        res="Fail"
-    else:
-        res="Pass"
+    try:
+        if score <50:
+            res="Fail"
+        else:
+            res="Pass"
+    except TypeError:
+        return "Please, input a valid score."
     return res
-print(pass_or_fail(average_args(45,56,34,67,76,87,34,56)))
-print(average_args(45,56,34,67,76,87,34,56))
+print(pass_or_fail(average_args(45,56,76,67,76,87,34,56)))
+print(average_args(45,56,76,67,76,87,34,56))
 
