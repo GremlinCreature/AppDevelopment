b8aef12f4c7e74da82f0c71fe95a4a701ebbc7c5
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-04-10 11:46:55 +1200

    Added a space

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -1,4 +1,4 @@
 import shoppint_calculator
 
 print(shoppint_calculator.discount(Banana=20.0, Orange=30.0, AntiImposterRepelent=50.0,Amongu=34.0))
-print(shoppint_calculator.purchases_total(shoppint_calculator.discount(Banana=20.0, Orange=30.0, AntiImposterRepelent=50.0,Amongu=34.0)))
+print(shoppint_calculator.purchases_total(shoppint_calculator.discount(Banana=20.0, Orange=30.0, AntiImposterRepelent=50.0, Amongu=34.0)))
